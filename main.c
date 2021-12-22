bc34a5502350fc59d0c838fa94bc98e3b1bb00a0
Zhang Huanghui <zhanghuanghui1007@163.com>
2021-12-22 15:03:20 +0800

    Revert "Prompt user for last name as well."
    This reverts commit 618c9c3b5305bd4728ee6eb99df9df6b53977700.
    
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 
 int main(int argc, char **argv)
 {
+<<<<<<< HEAD
     printf("Hello world!\n");
+=======
+    char name[255];
+
+    fgets(name, 255, stdin);
+    name[strlen(name)-1] = '\0';/* remove the newline at the end */
+
+    printf("Hello %s!\n", name);
+>>>>>>> parent of 618c9c3 (Prompt user for last name as well.)
     return 0;
 }
